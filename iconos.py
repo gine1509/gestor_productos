4006b9bf00bac4a803452596d335aa4589706eeb
Gine <ginecompu@gmail.com>
2024-08-22 20:36:33 -0400

    actualizando la ruta de los iconos

--- iconos.py
+++ iconos.py
@@ -2,47 +2,47 @@ from tkinter import PhotoImage
 
 #Ventana principal
 #icono principal
-principal=PhotoImage(file="iconos\icono_principal.png")
+principal=PhotoImage(file="gestor_productos\iconos\icono_principal.png")
 #icono de encabezado
-encabezado_icono=PhotoImage(file="iconos\icono_encabezado.png")
+encabezado_icono=PhotoImage(file="gestor_productos\iconos\icono_encabezado.png")
 #icono de la hora
-hora=PhotoImage(file="iconos\Reloj.png")
+hora=PhotoImage(file="gestor_productos\iconos\Reloj.png")
 #icono de la fecha
-fecha=PhotoImage(file="iconos\Fecha.png")
+fecha=PhotoImage(file="gestor_productos\iconos\Fecha.png")
 
 #cuerpo
 #icono del boton de agregar producto
-agregar=PhotoImage(file="iconos\Agregar_producto.png")
+agregar=PhotoImage(file="gestor_productos\iconos\Agregar_producto.png")
 #icono del boton buscar
-buscar=PhotoImage(file="iconos\Buscar.png")
+buscar=PhotoImage(file="gestor_productos\iconos\Buscar.png")
 #icono del boton editar
-editar=PhotoImage(file="iconos\Editar.png")
+editar=PhotoImage(file="gestor_productos\iconos\Editar.png")
 #icono del boton eliminar
-eliminar=PhotoImage(file="iconos\Eliminar.png")
+eliminar=PhotoImage(file="gestor_productos\iconos\Eliminar.png")
 #icono de boton generar reporte
-reporte=PhotoImage(file="iconos\generar_reporte.png")
+reporte=PhotoImage(file="gestor_productos\iconos\generar_reporte.png")
 #icono de boton actualizar cantidad
-actualizar=PhotoImage(file="iconos\Actualizar_cantidad.png")
+actualizar=PhotoImage(file="gestor_productos\iconos\Actualizar_cantidad.png")
 #icono de boton de salir
-salir=PhotoImage(file="iconos\salir.png")
+salir=PhotoImage(file="gestor_productos\iconos\salir.png")
 
 #formulario de agregar producto
 #icono de ventana
-icono_ventana=PhotoImage(file="iconos\icono_agregar_producto_form.png")
+icono_ventana=PhotoImage(file="gestor_productos\iconos\icono_agregar_producto_form.png")
 #ventana de categoria
 #icono para agregar categoria
-categoria_icono=PhotoImage(file="iconos\Agregar.png")
+categoria_icono=PhotoImage(file="gestor_productos\iconos\Agregar.png")
 #icono de salir en categorias
-salir_cat=PhotoImage(file="iconos\Flecha_salir.png")
+salir_cat=PhotoImage(file="gestor_productos\iconos\Flecha_salir.png")
 #buscar categoria
-buscar_cat=PhotoImage(file="iconos\Buscar_categoria.png")
+buscar_cat=PhotoImage(file="gestor_productos\iconos\Buscar_categoria.png")
 #editar categoria
-editar_cat=PhotoImage(file="iconos\Editar_categoria.png")
+editar_cat=PhotoImage(file="gestor_productos\iconos\Editar_categoria.png")
 #eliminar categoria
-eliminar_cat=PhotoImage(file="iconos\Eliminar_categoria.png")
+eliminar_cat=PhotoImage(file="gestor_productos\iconos\Eliminar_categoria.png")
 
 #ventana actualizar cantidad
-icono_actualizar=PhotoImage(file="iconos\Actualizar_ventana_icono.png")
-entrada=PhotoImage(file="iconos\Flecha_entrada.png")
-salida=PhotoImage(file="iconos\Flecha_salida.png")
+icono_actualizar=PhotoImage(file="gestor_productos\iconos\Actualizar_ventana_icono.png")
+entrada=PhotoImage(file="gestor_productos\iconos\Flecha_entrada.png")
+salida=PhotoImage(file="gestor_productos\iconos\Flecha_salida.png")
 
